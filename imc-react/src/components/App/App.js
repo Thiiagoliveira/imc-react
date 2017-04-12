ca6f02a511f26115697a5653ebd7b5ddf05c5446
Thiago.santos <thiago.bsb@hotmail.com>
2017-04-12 14:41:58 -0300

    imc.v2

--- imc-react/src/components/App/App.js
+++ imc-react/src/components/App/App.js
@@ -4,19 +4,49 @@ import '../../mkdir/side-menu.css';
 
 
 class App extends Component {
-  
-  constructor(){
+  constructor(props) {
     super();
-    this.state = {lista: [{
+    this.state = {
+      lista: [],
+      item: {
         nome: '',
         peso: '',
-        altura: '',
-        imc: 0
-      }]
+        altura: ''
+      }
     }
   }
+  handleChange = () => {
+    this.setState({ lista: this.state.lista.concat([this.state.item]) });
+    this.setState({
+      item: {
+        nome: '',
+        peso: '',
+        altura: ''
+      }
+    })
+    console.log(this.state.lista)
+  }
+  handleNome = (event) => {
+    var item = Object.assign({}, this.state.item, { nome: event.target.value });
+    this.setState({ item });
+  }
+
+  handlePeso = (event) => {
+    var item = Object.assign({}, this.state.item, { peso: event.target.value });
+    this.setState({ item });
+  }
+
+  handleAltura = (event) => {
+    var item = Object.assign({}, this.state.item, { altura: event.target.value });
+    this.setState({ item });
+  }
+
 
   render() {
+    var nome = this.state.item.nome;
+    var peso = this.state.item.peso;
+    var altura = this.state.item.altura;
+
     return (
       <div id="main">
         <div className="header">
@@ -26,24 +56,24 @@ class App extends Component {
 
         <div className="content">
           <h2 className="content-subhead">Preencha seus dados</h2>
-          <form className="pure-form pure-form-aligned">
-            <div className="pure-control-group">
-              <label htmlFor="nome">Nome</label> 
-              <input id="nome" type="text" name="nome" value={this.state.lista.nome} onChange={this.handleChange}/>                  
-            </div>
-            <div className="pure-control-group">
-              <label htmlFor="peso">Peso</label> 
-              <input id="peso" type="text" name="peso" value={this.state.lista.peso} onChange={this.handleChange}/>                  
-            </div>
-            <div className="pure-control-group">
-              <label htmlFor="altura">Altura</label> 
-              <input id="altura" type="text" name="altura" value={this.state.lista.altura} onChange={this.handleChange}/>                                      
-            </div>
-            <div className="pure-control-group">                                  
-              <label></label> 
-               <button type="submit" onClick="this.handleChange" className="pure-button pure-button-success">Salvar</button>    
-            </div>
-          </form>
+
+          <div className="pure-control-group">
+            <label htmlFor="nome">Nome</label>
+            <input id="nome" type="text" name="nome" value={nome} onChange={this.handleNome} />
+          </div>
+          <div className="pure-control-group">
+            <label htmlFor="peso">Peso</label>
+            <input id="peso" type="text" name="peso" value={peso} onChange={this.handlePeso} />
+          </div>
+          <div className="pure-control-group">
+            <label htmlFor="altura">Altura</label>
+            <input id="altura" type="text" name="altura" value={altura} onChange={this.handleAltura} />
+          </div>
+          <div className="pure-control-group">
+            <label></label>
+            <button onClick={this.handleChange} className="pure-button pure-button-success">Salvar</button>
+          </div>
+
           <table className="pure-table">
             <thead>
               <tr>
@@ -55,20 +85,20 @@ class App extends Component {
             </thead>
             <tbody>
               {
-                this.state.lista.map(function(pessoa){
+                this.state.lista.map(function (pessoa) {
                   return (
                     <tr>
-                      <td>{pessoa.nome}</td>                
+                      <td>{pessoa.nome}</td>
                       <td>{pessoa.peso}</td>
                       <td>{pessoa.altura}</td>
-                      <td>{pessoa.imc}</td>                  
+                      <td>{pessoa.imc}</td>
                     </tr>
                   );
                 })
               }
-              
+
             </tbody>
-          </table> 
+          </table>
         </div>
       </div>
     );
